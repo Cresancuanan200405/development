b499997424b4a4fab02fe1cc71211b3472b45a3a
Cresancuanan200405 <cresan.cuanan@urios.edu.ph>
2025-05-09 12:18:28 +0000

    Initial commit

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s@#fyy5)w5!x+&8fcdt405^avhb9ry07p!i^mt-s-7(jd633i9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*'] 
 CORS_ALLOW_ALL_ORIGINS = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -90,8 +90,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Use MySQL engine
-        'NAME': 'default_db1',                 # Your database name
-        'USER': 'default',                     # Your MySQL username
+        'NAME': 'cuanan5_db1',                 # Your database name
+        'USER': 'cuanan5',                     # Your MySQL username
         'PASSWORD': '123456',                  # Your MySQL password
         'HOST': 'localhost',                   # MySQL server host (localhost if on the same machine)
         'PORT': '3306',                        # MySQL default port
@@ -118,8 +118,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = '/home/defaut/development/app/media/'
-FORCE_SCRIPT_NAME = '/default'
+MEDIA_ROOT = '/home/cuanan5/development/app/media/'
+FORCE_SCRIPT_NAME = '/cuanan5'
 USE_X_FORWARDED_HOST = True
 USE_X_FORWARDED_PORT = True
 
